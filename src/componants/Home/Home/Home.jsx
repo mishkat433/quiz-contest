70c4a72ad6c8686ed60e7f001c7ae3f61be6c930
miskat433 <miskat433@gmail.com>
2022-10-10 23:02:57 +0600

    header added

--- src/componants/Home/Home/Home.jsx
+++ src/componants/Home/Home/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import HeaderMain from '../../Header/HeaderMain/HeaderMain';
 
 
 const Home = () => {
     return (
         <div>
-
+            <HeaderMain />
 
         </div>
     );
