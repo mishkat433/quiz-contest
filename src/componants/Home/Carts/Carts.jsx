6f02016af1f9871d0480fe4dcad90781c131e0f8
miskat433 <miskat433@gmail.com>
2022-10-11 00:04:21 +0600

    spinner added

--- src/componants/Home/Carts/Carts.jsx
+++ src/componants/Home/Carts/Carts.jsx
@@ -1,8 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, CSSProperties } from 'react';
+import ClipLoader from "react-spinners/ClipLoader";
 import Cart from '../Cart/Cart';
+
+const override: CSSProperties = {
+    display: "block",
+    margin: "0 auto",
+    borderColor: "red",
+};
+
 
 const Carts = () => {
     const [carts, setCarts] = useState([]);
+    let [loading, setLoading] = useState(true);
 
     useEffect(() => {
         try {
@@ -13,11 +22,20 @@ const Carts = () => {
         catch (err) {
             console.log(err.message);
         }
-
+        finally {
+            setLoading(false)
+        }
     }, [])
     return (
         <div className='w-11/12 mx-auto'>
-            <h1 className='text-lg md:text-5xl mb-14 text-center font-semibold text-orange-600 underline'>Participate with...</h1>
+            <h1 className='text-lg md:text-5xl mb-14 text-center font-semibold text-orange-600 '>Participate with...</h1>
+            <ClipLoader
+                loading={loading}
+                cssOverride={override}
+                size={120}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+            />
             <div className=' grid  lg:grid-cols-2 gap-10'>
                 {
                     carts.map(cart => <Cart cart={cart} key={cart.id} />)
